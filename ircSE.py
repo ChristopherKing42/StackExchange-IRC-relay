57e995be3ca11140c9c7e5f280d3baa32ff72625
Christopher King <G.nius.ck@gmail.com>
2016-08-30 15:27:28 +0000

    Initial Commit

---
+++ ircSE.py
@@ -0,0 +1,54 @@
+import socket
+import sys
+
+import chatexchange.client
+import chatexchange.events
+
+server = "irc.freenode.org"       #settings
+channel = raw_input("IRC channel: ")
+botnick = raw_input("IRC bot nick: ")
+
+room_id = raw_input("Stack Exchange room id: ")
+email = raw_input("Stack Exchange bot email: ")
+password = raw_input("Stack Exchange bot password: ")
+
+#Based on code from http://stackoverflow.com/a/12219119/1172541
+
+irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #defines the socket
+print "connecting to:"+server
+irc.connect((server, 6667))                                                         #connects to the server
+irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This is a fun bot!\n") #user authentication
+irc.send("NICK "+ botnick +"\n")                            #sets nick
+irc.send("PRIVMSG nickserv :iNOOPE\r\n")    #auth
+irc.send("JOIN "+ channel +"\n")        #join the chan
+
+#Based on sample from https://github.com/Manishearth/ChatExchange/blob/master/examples/chat.py
+
+def on_message(message, client):
+    if not isinstance(message, chatexchange.events.MessagePosted):
+        # Ignore non-message_posted events.
+        return
+    print message
+    if message.content.startswith(':'):
+        return #a message from stack exchange
+    irc.send("PRIVMSG " + channel + ' :<' + message.user.name + '> ' + message.content + '\r\n')
+
+host_id = 'stackexchange.com'
+print "connecting to:"+host_id
+client = chatexchange.client.Client(host_id)
+client.login(email, password)
+
+room = client.get_room(room_id)
+room.join()
+room.watch(on_message)
+
+# Also from http://stackoverflow.com/a/12219119/1172541
+
+while True:    #puts it in a loop
+	text=irc.recv(2040)  #receive the text
+	room.send_message(text)
+
+	if text.find('PING') != -1:                          #check if 'PING' is found
+		irc.send('PONG ' + text.split() [1] + '\r\n') #returnes 'PONG' back to the server (prevents pinging out!)
+
+
